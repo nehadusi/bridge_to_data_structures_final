6df043c36f6b2c1b00a062fcadac69cc2223cd08
Neha Dusi <103844683+nehadusi@users.noreply.github.com>
2026-06-06 16:51:10 -0700

    Enhance Pokemon class with wins and medals tracking
    Added wins and medals attributes with corresponding methods.

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include "Pokemon.h"
 
 using namespace std;
@@ -7,10 +8,14 @@ Pokemon::Pokemon()
 {
     name = "";
     type = "";
+
     hp = 0;
     attack = 0;
     defense = 0;
     speed = 0;
+
+    wins = 0;
+    medals = 0;
 }
 
 Pokemon::Pokemon(string newName,
@@ -22,10 +27,14 @@ Pokemon::Pokemon(string newName,
 {
     name = newName;
     type = newType;
+
     hp = newHp;
     attack = newAttack;
     defense = newDefense;
     speed = newSpeed;
+
+    wins = 0;
+    medals = 0;
 }
 
 string Pokemon::getName() const
@@ -58,18 +67,64 @@ int Pokemon::getSpeed() const
     return speed;
 }
 
+int Pokemon::getWins() const
+{
+    return wins;
+}
+
+int Pokemon::getMedals() const
+{
+    return medals;
+}
+
 void Pokemon::setHp(int newHp)
 {
     hp = newHp;
 }
 
+void Pokemon::addWin()
+{
+    wins++;
+}
+
+void Pokemon::addMedal()
+{
+    medals++;
+}
+
 void Pokemon::displayPokemon() const
 {
     cout << endl;
-    cout << "Name: " << name << endl;
-    cout << "Type: " << type << endl;
-    cout << "HP: " << hp << endl;
-    cout << "Attack: " << attack << endl;
-    cout << "Defense: " << defense << endl;
-    cout << "Speed: " << speed << endl;
+
+    cout << "Name: "
+         << name
+         << endl;
+
+    cout << "Type: "
+         << type
+         << endl;
+
+    cout << "HP: "
+         << hp
+         << endl;
+
+    cout << "Attack: "
+         << attack
+         << endl;
+
+    cout << "Defense: "
+         << defense
+         << endl;
+
+    cout << "Speed: "
+         << speed
+         << endl;
+
+    cout << "Wins: "
+         << wins
+         << endl;
+
+    cout << "Medals: "
+         << medals
+         << endl;
 }
