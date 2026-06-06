e79f33dfaf458ecd0cd6e414f297dc7659eb1d53
Neha Dusi <103844683+nehadusi@users.noreply.github.com>
2026-06-06 16:48:15 -0700

    Refactor BattleSystem and add Pokemon class

--- BattleSystem.cpp
+++ BattleSystem.cpp
@@ -1,183 +1,53 @@
-#include <iostream>
-#include <string>
+#ifndef POKEMON_H
+#define POKEMON_H
 
-#include "BattleSystem.h"
+#include <string>
 
 using namespace std;
 
-BattleSystem::BattleSystem()
+class Pokemon
 {
-}
+private:
 
-int BattleSystem::calculateDamage(
-    const Pokemon & attacker,
-    const Pokemon & defender)
-{
-    int damage =
-        attacker.getAttack()
-        - defender.getDefense();
+    string name;
+    string type;
 
-    if (damage < 1)
-    {
-        damage = 1;
-    }
+    int hp;
+    int attack;
+    int defense;
+    int speed;
 
-    return damage;
-}
+    int wins;
+    int medals;
 
-void BattleSystem::recordBattleAction(
-    const string & action)
-{
-    battleHistory.push(action);
-}
+public:
 
-void BattleSystem::startBattle(
-    Pokemon playerPokemon,
-    Pokemon enemyPokemon)
-{
-    if (
-        playerPokemon.getSpeed()
-        >= enemyPokemon.getSpeed())
-    {
-        battleQueue.enqueue(
-            playerPokemon);
+    Pokemon();
 
-        battleQueue.enqueue(
-            enemyPokemon);
-    }
-    else
-    {
-        battleQueue.enqueue(
-            enemyPokemon);
+    Pokemon(string newName,
+            string newType,
+            int newHp,
+            int newAttack,
+            int newDefense,
+            int newSpeed);
 
-        battleQueue.enqueue(
-            playerPokemon);
-    }
+    string getName() const;
+    string getType() const;
 
-    int playerHp =
-        playerPokemon.getHp();
+    int getHp() const;
+    int getAttack() const;
+    int getDefense() const;
+    int getSpeed() const;
 
-    int enemyHp =
-        enemyPokemon.getHp();
+    int getWins() const;
+    int getMedals() const;
 
-    cout << endl;
-    cout << "Battle Start!" << endl;
-    cout << playerPokemon.getName()
-         << " VS "
-         << enemyPokemon.getName()
-         << endl;
+    void setHp(int newHp);
 
-    while (
-        playerHp > 0
-        &&
-        enemyHp > 0)
-    {
-        Pokemon attacker =
-            battleQueue.dequeue();
+    void addWin();
+    void addMedal();
 
-        if (
-            attacker.getName()
-            ==
-            playerPokemon.getName())
-        {
-            int damage =
-                calculateDamage(
-                    playerPokemon,
-                    enemyPokemon);
+    void displayPokemon() const;
+};
 
-            enemyHp =
-                enemyHp - damage;
-
-            string action =
-                playerPokemon.getName()
-                +
-                " attacked "
-                +
-                enemyPokemon.getName();
-
-            recordBattleAction(
-                action);
-
-            cout << action
-                 << endl;
-
-            if (enemyHp > 0)
-            {
-                battleQueue.enqueue(
-                    playerPokemon);
-            }
-        }
-        else
-        {
-            int damage =
-                calculateDamage(
-                    enemyPokemon,
-                    playerPokemon);
-
-            playerHp =
-                playerHp - damage;
-
-            string action =
-                enemyPokemon.getName()
-                +
-                " attacked "
-                +
-                playerPokemon.getName();
-
-            recordBattleAction(
-                action);
-
-            cout << action
-                 << endl;
-
-            if (playerHp > 0)
-            {
-                battleQueue.enqueue(
-                    enemyPokemon);
-            }
-        }
-    }
-
-    cout << endl;
-
-    if (playerHp > 0)
-    {
-        cout
-            << playerPokemon.getName()
-            << " wins!"
-            << endl;
-    }
-    else
-    {
-        cout
-            << enemyPokemon.getName()
-            << " wins!"
-            << endl;
-    }
-}
-
-void BattleSystem::displayBattleHistory()
-{
-    Stack tempStack;
-
-    while (
-        !battleHistory.isEmpty())
-    {
-        string action =
-            battleHistory.pop();
-
-        cout
-            << action
-            << endl;
-
-        tempStack.push(
-            action);
-    }
-
-    while (
-        !tempStack.isEmpty())
-    {
-        battleHistory.push(
-            tempStack.pop());
-    }
-}
+#endif
